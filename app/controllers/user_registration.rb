070dcf445af61051c4032408441b9eabef6c7b12
Sebastian Buza <gore.sexyx@gmail.com>
2015-03-26 07:50:46 +0200

    fix capybara

---
+++ app/controllers/user_registration.rb
@@ -0,0 +1,5 @@
+class UserRegistration < ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get '/user_registration', to: 'user_registration#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
